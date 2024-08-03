ff79e8300550fae95b3bc9ea76e44276230d5482
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-08-03 19:33:45 +0530

    feat:added error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import LandingPageView from "@/components/user/landingPage/LandingPage";
 import ProfilePage from "@/components/user/landingPage/LandingProfilePage";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
--- src/app/(home)/template.tsx
+++ src/app/(home)/template.tsx
@@ -1,37 +1,36 @@
-"use client";
+  "use client";
 
-import Aos from "aos";
-import { useEffect, useState } from "react";
-import "aos/dist/aos.css";
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "@/store/storeProviders/ThemeProvider";
-import dynamic from "next/dynamic";
-import { useUserStore } from "@/store/storeProviders/UseUserStore";
+  import Aos from "aos";
+  import { useEffect, useState } from "react";
+  import "aos/dist/aos.css";
 
-const ParticlesComponent = dynamic(
-  () => import("@/services/providers/ParticleProvider"),
-  { ssr: false }
-);
+  import dynamic from "next/dynamic";
+  import { useUserStore } from "@/store/storeProviders/UseUserStore";
 
-export default function Template({ children }: { children: React.ReactNode }) {
-  const [isClient, setIsClient] = useState(false);
-  const isAuthenticated = useUserStore((state) => state.isAuthenticated);
-
-  useEffect(() => {
-    setIsClient(true);
-    Aos.init();
-  }, []);
-
-  return (
-    <SessionProvider>
-      {isClient && !isAuthenticated && <ParticlesComponent id="particlejs" />}
-      <div
-        className={`relative z-10 ${
-          isAuthenticated ? "bg-white text-black" : ""
-        }`}
-      >
-        {children}
-      </div>
-    </SessionProvider>
+  const ParticlesComponent = dynamic(
+    () => import("@/services/providers/ParticleProvider"),
+    { ssr: false }
   );
-}
+
+  export default function Template({ children }: { children: React.ReactNode }) {
+    const [isClient, setIsClient] = useState(false);
+    const isAuthenticated = useUserStore((state) => state.isAuthenticated);
+
+    useEffect(() => {
+      setIsClient(true);
+      Aos.init();
+    }, []);
+
+    return (
+      <>
+        {isClient && !isAuthenticated && <ParticlesComponent id="particlejs" />}
+        <div
+          className={`relative z-10 ${
+            isAuthenticated ? "bg-white text-black" : ""
+          }`}
+        >
+          {children}
+        </div>
+      </>
+    );
+  }
